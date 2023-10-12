854f4f10e2e66853c37a0a608c7c6dd18c789bbd
Serhii Kostrykin <serhii.kostrykin@au10tix.com>
2023-10-13 00:50:16 +0100

    Au10tixCore

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Au10tixCore",
+    platforms: [
+            .iOS(.v11)
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Au10tixCore",
+            targets: ["Au10tixCore"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "Au10tixCore", url: "https://github.com/au10tixmobile/iOS_Distribution_/Au10tixCore.xcframework.zip", checksum: "f68125552485b4b7f6420b552ca254c5e00ef6e891cd76bf1d0f5fda448c66dd")
+    ]
+)
